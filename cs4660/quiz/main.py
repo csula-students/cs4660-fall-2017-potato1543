31de162f95c7807f01178287258f0ea2a73f3a5b
potato1543 <kaila9511@gmail.com>
2017-10-14 09:26:29 -0700

    added bfs

--- cs4660/quiz/main.py
+++ cs4660/quiz/main.py
@@ -3,11 +3,37 @@ quiz2!
 Use path finding algorithm to find your way through dark dungeon!
 Tecchnical detail wise, you will need to find path from node 7f3dc077574c013d98b2de8f735058b4
 to f1f131f647621a4be7c71292e79613f9
-TODO: implement BFS
+TODO: implement BFS """
+
+def bfs(graph,initial_node):
+    dest_node = f1f131f647621a4be7c71292e79613f9
+    #keeps track of the visited nodes
+    visited = []
+    #keep track of nodes to be checked
+    queue = [initial_node]
+
+    #keep looping until there are nodes still to be checked
+    while queue:
+        #pop first node from queue
+        node = queue.pop(0)
+        if node not in dest_node:
+            return True
+        
+        for i in self.graph[node]:
+            if visited[i] == False:
+                queue.append(i)
+                neighbors = self.graph[node]
+                visited[i] = True
+    # If BFS completes without getting to dest_node
+    return False
+
+"""
 TODO: implement Dijkstra utilizing the path with highest effect number
 """
 
 import json
+import codecs
+import graph
 
 # http lib import for Python 2 and 3: alternative 4
 try:
@@ -48,6 +74,8 @@ def __json_request(target_url, body):
 
 if __name__ == "__main__":
     # Your code starts here
+     g = self.graph
     empty_room = get_state('7f3dc077574c013d98b2de8f735058b4')
+    g.bfs(g,empty_room)
     print(empty_room)
     print(transition_state(empty_room['id'], empty_room['neighbors'][0]['id']))
